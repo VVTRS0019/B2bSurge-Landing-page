e043ea6104b29c39def26b539a5c957e69a5cb09
ttushaar1906 <ttushaar45@gmail.com>
2025-08-25 11:27:48 +0530

    basic setup and git test

---
+++ src/app/termsofservice/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main>
+        <section className='border'>
+                <h2>Terms of Service</h2>
+                <h5>Last updated: January 15, 2025</h5>
+        </section>
+    </main>
+  )
+}
